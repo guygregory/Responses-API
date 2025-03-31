edeed576fd214bec5de48d8b2f94b8e6bcdd33a1
Guy Gregory <register@guygregory.com>
2025-03-31 16:23:29 +0100

    Add files via upload

---
+++ responses-structured-aoai.py
@@ -0,0 +1,51 @@
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+client = AzureOpenAI(
+    api_key = os.environ["AZURE_OPENAI_API_KEY"],  
+    api_version = os.environ["AZURE_OPENAI_API_VERSION"],
+    azure_endpoint = os.environ["AZURE_OPENAI_API_ENDPOINT"]
+    )
+
+
+response = client.responses.create(
+    model=os.environ["AZURE_OPENAI_API_MODEL"],
+    input=[
+        {"role": "system", "content": "Extract the event information."},
+        {"role": "user", "content": "Alice and Bob are going to a science fair on Friday."}
+    ],
+    text={
+        "format": {
+            "type": "json_schema",
+            "name": "calendar_event",
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string"
+                    },
+                    "date": {
+                        "type": "string"
+                    },
+                    "participants": {
+                        "type": "array", 
+                        "items": {
+                            "type": "string"
+                        }
+                    },
+                },
+                "required": ["name", "date", "participants"],
+                "additionalProperties": False
+            },
+            "strict": True
+        }
+    }
+)
+
+event = json.loads(response.output_text)
+
+print(event)
